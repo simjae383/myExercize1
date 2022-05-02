6b508d8279636bab13c42886fa810007e727b496
Sim981 <nepp.plus@gmail.com>
2022-05-02 18:29:15 +0900

    색상 변경

--- app/src/main/java/com/example/myexercize1/MainActivity.kt
+++ app/src/main/java/com/example/myexercize1/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.myexercize1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createBtn.setOnClickListener{
+            Log.d("MainActivity_회원가입","회원가입")
+        }
+
+        loginBtn.setOnClickListener {
+            Toast.makeText(this,"로그인을 환영합니다.",Toast.LENGTH_SHORT).show()
+        }
     }
 }
